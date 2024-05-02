8884335e82276827d5e763f6fab56d60a5124e5a
Nafiz_ <nafizuddin1537@gmail.com>
2024-05-02 22:24:43 +0600

    Automated Messege

---
+++ Autowhatspp.py
@@ -0,0 +1,5 @@
+import pywhatkit
+pywhatkit.sendwhatmsg("+88 target number", "messege", Hour, miniutes,)
+
+#frist off all you need to download the python pakage  "pywhatkit"
+#then write your notes!
